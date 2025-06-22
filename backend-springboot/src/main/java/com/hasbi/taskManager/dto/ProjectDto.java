cd2a779aea0c1b26214bf20b7cef242b56e55181
FatimaZahraHASBI <hasbi.fatimazahra04@gmail.com>
2025-06-22 17:13:49 +0100

    Add DTOs

---
+++ backend-springboot/src/main/java/com/hasbi/taskManager/dto/ProjectDto.java
@@ -0,0 +1,14 @@
+package com.hasbi.taskManager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectDto {
+    private Long id;
+    private String name;
+    private String description;
+}
