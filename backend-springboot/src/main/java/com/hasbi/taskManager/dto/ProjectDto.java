0aa407d9809f77d1516e29b0379d16e54e2444e5
FatimaZahraHASBI <hasbi.fatimazahra04@gmail.com>
2025-06-24 00:00:30 +0100

    Add validation to DTOs

--- backend-springboot/src/main/java/com/hasbi/taskManager/dto/ProjectDto.java
+++ backend-springboot/src/main/java/com/hasbi/taskManager/dto/ProjectDto.java
@@ -1,5 +1,6 @@
 package com.hasbi.taskManager.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProjectDto {
     private Long id;
+    @NotBlank(message = "Name is required")
     private String name;
     private String description;
 }
--- backend-springboot/src/main/java/com/hasbi/taskManager/dto/TaskDto.java
+++ backend-springboot/src/main/java/com/hasbi/taskManager/dto/TaskDto.java
@@ -1,5 +1,6 @@
 package com.hasbi.taskManager.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class TaskDto {
     private Long id;
+    @NotBlank(message = "Title is required")
     private String title;
     private String description;
+    @NotBlank(message = "Status is required")
     private String status;
     private String priority;
     private Long projectId;
