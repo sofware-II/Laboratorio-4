a02fc944872e21d5a1fa434c1081c726d2f004be
Marcelo Surco <surcos2002@gmail.com>
2026-06-30 02:33:46 -0500

    refactor: migrate Project module to a clean architecture structure

--- backend-springboot/src/main/java/com/hasbi/taskmanager/repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hasbi.taskmanager.repository;
-
-import com.hasbi.taskmanager.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
